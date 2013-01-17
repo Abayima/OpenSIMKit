9f88367bc8f2c838b8cc0e0482690734e13de7f7
Matt Griffiths <mrmatthewgriffiths@gmail.com>
2013-01-17 15:15:44 -0500

    Restructured Repo

--- Code/SimKit/Properties/AssemblyInfo.cs
+++ Code/SimKit/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SimKit")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("OSK")]
+[assembly: AssemblyDescription("Open Sim Kit (OSK) is FOSS software for editing the content of SIM cards.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimKit")]
+[assembly: AssemblyCompany("Abayima.com")]
+[assembly: AssemblyProduct("OSK")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
